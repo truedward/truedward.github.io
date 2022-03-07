1f49824f130965c24552166f6be2ed1d5a3ef7d5
trueed <true.a.e.p.o@gmail.com>
2022-03-07 23:08:54 +0200

    desktop header layout

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="header"></div>
+  <div class="header">
+    <div class="header__inner">
+      <div class="header__language-switcher">
+        <div
+          class="header__language-switcher-item header__language-switcher-item_active"
+        >
+          UA
+        </div>
+        <div class="header__language-switcher-item">EN</div>
+      </div>
+      <div class="header__contacts-btn">
+        <div class="header__contacts-btn-bg-line_1"></div>
+        <div class="header__contacts-btn-bg-line_2"></div>
+        <div class="header__contacts-btn-bg-line_3"></div>
+        CONTACTS
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
