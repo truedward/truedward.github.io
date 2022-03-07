5afe8cda0e0308035e61352a1af556240c462d69
trueed <true.a.e.p.o@gmail.com>
2022-03-07 20:16:56 +0200

    Init vue project

---
+++ src/layout/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {
+  // @Prop() private msg!: string;
+}
+</script>
+
+<style lang="scss"></style>
