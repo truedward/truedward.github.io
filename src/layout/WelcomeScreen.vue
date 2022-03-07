c329f047fff38bfd6a0f39d246303b752a11c38a
trueed <true.a.e.p.o@gmail.com>
2022-03-08 00:26:44 +0200

    Desktop welcome screen layout

---
+++ src/layout/WelcomeScreen.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="welcome-screen">
+    <div class="welcome-screen__inner">
+      <div class="welcome-screen__photo">
+        <div class="welcome-screen__photo-title">
+          EDWARD PRYKHOZHAY
+          <div class="welcome-screen__photo-title-underline"></div>
+        </div>
+        <img
+          class="welcome-screen__photo-photo"
+          src="@/assets/my-face.png"
+          alt=""
+        />
+      </div>
+      <div class="welcome-screen__additional">
+        <img
+          class="welcome-screen__additional-arrows"
+          src="@/assets/main-arrows.svg"
+          alt=""
+        />
+        <div class="welcome-screen__additional-title">
+          <span>MIDDLE</span> FRONT-END DEVELOPER
+        </div>
+      </div>
+      <div class="welcome-screen__mouse-navigation"></div>
+    </div>
+  </div>
+</template>
