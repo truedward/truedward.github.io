c9d901702ce28d9905b80727a2bd80e579dbc721
truedward <truedward.github.io>
2022-03-12 22:11:20 +0200

    Adding links to the contacts block

--- src/layout/Contacts.vue
+++ src/layout/Contacts.vue
@@ -7,18 +7,28 @@
     />
     <div class="contacts__list">
       <span class="contacts__list-title">EMAIL:</span>
-      <a class="contacts__list-item" href="#">
+      <a class="contacts__list-item" href="mailto:true.ed@icloud.com">
         true.ed@icloud.com
         <span class="contacts__list-item-tip">priority</span>
       </a>
-      <a class="contacts__list-item" href="#">TRUE.A.E.P.O@Gmail.com</a>
+      <a class="contacts__list-item" href="mailto:TRUE.A.E.P.O@Gmail.com"
+        >TRUE.A.E.P.O@Gmail.com</a
+      >
       <span class="contacts__list-title">OTHER:</span>
-      <a class="contacts__list-item" href="#">linkedin</a>
-      <a class="contacts__list-item" href="#">GITHUB</a>
-      <a class="contacts__list-item" href="#">
+      <a
+        class="contacts__list-item"
+        href="https://www.linkedin.com/in/edward-prykhozhay/"
+        >linkedin</a
+      >
+      <a class="contacts__list-item" href="https://github.com/truedward/"
+        >GITHUB</a
+      >
+      <a class="contacts__list-item" href="https://t.me/trueed">
         TELEGRAM <span class="contacts__list-item-tip">priority</span>
       </a>
-      <a class="contacts__list-item" href="#">INSTAGRAM</a>
+      <a class="contacts__list-item" href="https://www.instagram.com/true.ed/"
+        >INSTAGRAM</a
+      >
     </div>
   </div>
 </template>
